1acd481ce25ba108047d726b4eca1f9e4d3ec3b4
NilDebug <toob@vip.qq.com>
2023-05-22 19:22:36 +0800

    Update common.go

--- tools/common.go
+++ tools/common.go
@@ -23,3 +23,50 @@ func RandomInt(min, max int) int {
 
 	return randomInt
 }
+
+// GetLocalIP
+//
+//	@Description: 获取本地IP地址
+//	@return string
+//	@return error
+func GetLocalIP() (string, error) {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "", err
+	}
+
+	for _, addr := range addrs {
+		ipnet, ok := addr.(*net.IPNet)
+		if ok && !ipnet.IP.IsLoopback() && ipnet.IP.To4() != nil {
+			return ipnet.IP.String(), nil
+		}
+	}
+
+	return "", fmt.Errorf("no local IP address found")
+}
+
+// GetLocalPort
+//
+//	@Description: 随机获取一个可用的端口
+//	@return int
+//	@return error
+func GetLocalPort() (int, error) {
+	listener, err := net.Listen("tcp", ":0")
+	if err != nil {
+		return 0, err
+	}
+	defer listener.Close()
+
+	addr := listener.Addr().(*net.TCPAddr)
+	return addr.Port, nil
+}
+
+// MD5
+//
+//	@Description: 获取MD5
+//	@param str
+//	@return string
+func MD5(str string) string {
+	hash := md5.Sum([]byte(str))
+	return hex.EncodeToString(hash[:])
+}
